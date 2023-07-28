a9fd16f7592712064318fbf3ad8e1a0e1696f08e
Franz Amersian <114055827+versacodes@users.noreply.github.com>
2023-07-28 15:18:57 +0800

    Created base component for button BaseButton Added some styling to BaseButton instance

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script setup>
 import NotesListSection from "./components/NotesListSection.vue"
+import BaseButton from "./components/BaseButton.vue"
 </script>
 
 <template>
   <h1 class="text-zinc-900 font-bold text-4xl mb-5">notes</h1>
   <NotesListSection />
+  <BaseButton
+    name_btn="New Note"
+    class="px-4 py-2 bg-[#649] text-white text-lg mt-3 font-semibold"
+  />
 </template>
 
 <style scoped>
---
+++ src/components/BaseButton.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps({
+  name_btn: String
+})
+</script>
+
+<template>
+  <button>{{name_btn}}</button>
+</template>
+
+<style scoped>
+
+</style>
