1410292cefddba0328726ece36d5fb7b87ed6054
Franz Amersian <114055827+versacodes@users.noreply.github.com>
2023-07-29 23:43:24 +0800

    Styled NotesOpen component

--- src/components/NotesOpen.vue
+++ src/components/NotesOpen.vue
@@ -1,13 +1,20 @@
 <script setup>
 import { useNotesStore } from '../store/notesStore.js'
+import BaseButton from './BaseButton.vue'
 
 const store = useNotesStore()
+
 </script>
 
 <template>
-<h1>Note</h1>
-<h2>{{ store.notes_array[store.open_note_index].title }}</h2>
-<p>{{ store.notes_array[store.open_note_index].content }}</p>
+  <h1 class="text-4xl font-bold mb-4">Note</h1>
+  <div class="w-full bg-white mx-auto h-[20rem] overflow-y-scroll break-words p-6">
+    <h2 class="font-bold text-3xl mb-6 text-stone-800">{{ store.notes_array[store.open_note_index].title }}</h2>
+    <p class="text-stone-700 text-md">{{ store.notes_array[store.open_note_index].content }}</p>
+  </div>
+  <BaseButton class="px-4 py-2 bg-[#649] text-white text-lg mx-auto mt-3 font-semibold w-full">
+  Back to Notes
+  </BaseButton>
 </template>
 
 <style scoped>
